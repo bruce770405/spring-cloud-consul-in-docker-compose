6ef787cb50ef425b2a7d1f726d1c16208f2a0ba3
Bruce Hsu <bruce770405@gmail.com>
2020-04-02 23:18:35 +0800

    修正config server的讀取錯誤問題

---
+++ config-server/src/main/java/tw/com/bruce/configserver/controller/TriggerController.java
@@ -0,0 +1,13 @@
+package tw.com.bruce.configserver.controller;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 
+ * @author BruceHsu
+ *
+ */
+@Component
+public class TriggerController {
+
+}
